d0774704e5ff07865b95de870ada862f67366237
shohag0007 <merinedrive007@gmail.com>
2024-03-06 18:52:29 +0600

    first commit

---
+++ fundamental.js
@@ -0,0 +1,15 @@
+// how to declare a variable using let and const
+const fatherName = 'shohag';
+let season = 'rainy';
+season = 'winter';
+
+// 6 basic conditions >, <, ===, !==, <=, <= 
+// multiple conditions: &&, ||
+
+if(fatherName === 'Shohag' || season === 'rainy'){
+
+}
+
+else{
+    
+}
